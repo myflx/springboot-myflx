7d5a31e1878b6724acf2cb14421c1cbe1725a1de
luoshanglin <ShanglinLuo@sf-express.com>
2020-01-08 09:44:33 +0800

    add md file

---
+++ myflx-servlet/RFC.md
@@ -0,0 +1,14 @@
+# RFC
+
+### 概念
+
+[RFC](https://baike.baidu.com/item/RFC/2798645?fr=aladdin)： 是由国际互联网工程任务组（IETF）收集的有关互联网的相关信息，以及[UNIX](https://baike.baidu.com/item/UNIX)和互联网[社群](https://baike.baidu.com/item/社群)的软件文件，以编号排定，目前RFC文件是由[互联网协会](https://baike.baidu.com/item/互联网协会)（ISOC）[赞助](https://baike.baidu.com/item/赞助)发行。
+
+### RFC下载
+
+https://tools.ietf.org/html/rfc7230
+
+
+
+https://www.rfc-editor.org/
+
--- myflx-validation/src/main/java/com/myflx/advice/ErrorHandleAdvice1.java
+++ myflx-validation/src/main/java/com/myflx/advice/ErrorHandleAdvice1.java
@@ -1,6 +1,8 @@
 package com.myflx.advice;
 
 import com.google.common.collect.Lists;
+import com.myflx.dto.ModelResult;
+import com.myflx.dto.ModelResultClient;
 import org.springframework.core.annotation.Order;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,12 +20,15 @@ import static com.myflx.advice.ErrorHandlerOrder.FIRST_LEVEL;
 @RestControllerAdvice
 public class ErrorHandleAdvice1 {
 
-    @ExceptionHandler(ConstraintViolationException.class)
-    public static String validHandler(ConstraintViolationException e) {
-        final List<ConstraintViolation> objects = Lists.newArrayList();
-        objects.addAll(e.getConstraintViolations());
-        /*System.out.println(1/0);*/
-        return objects.get(0).getMessage();
+    private ErrorHandleAdvice1() {
     }
+
+    /*@ExceptionHandler(ConstraintViolationException.class)
+    public static ModelResult<Boolean> validHandler(ConstraintViolationException e) {
+        final List<ConstraintViolation> violations = Lists.newArrayList(e.getConstraintViolations());
+        *//*log.info("校验不通过：{}", violations.get(0).getRootBeanClass().toString() + violations.get(0).getPropertyPath());*//*
+        System.out.println("校验不通过：{}" + violations.get(0).getRootBeanClass().toString() +"#"+ violations.get(0).getPropertyPath()+ violations.get(0).getMessage());
+        return new ModelResultClient<Boolean>().failFactory("1007", violations.get(0).getMessage());
+    }*/
 
 }
