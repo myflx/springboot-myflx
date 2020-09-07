23df0f1f4c69f8037eb1795a439c7a10de6ce2fd
luoshanglin <ShanglinLuo@sf-express.com>
2020-09-07 17:05:29 +0800

    题目，细节梳理

---
+++ myflx-validation/src/test/java/FinalizeEscapeGC.java
@@ -0,0 +1,45 @@
+/**
+ * @Description TODO
+ * @Author LuoShangLin
+ * @Date 2020/8/13 10:46
+ * @Since V2.13.0
+ */
+public class FinalizeEscapeGC {
+    public static FinalizeEscapeGC SAVE_HOOK = null;
+    public void isAlive() {
+        System.out.println("yes, i am still alive :)");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        System.out.println("finalize mehtod executed!");
+        FinalizeEscapeGC.SAVE_HOOK = this;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        SAVE_HOOK = new FinalizeEscapeGC();
+
+        //对象第一次成功拯救自己
+        SAVE_HOOK = null;
+        System.gc();
+        //因为finapze方法优先级很低，所以暂停0.5秒以等待它
+        Thread.sleep(500);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("no, i am dead :(");
+        }
+
+        //对象第一次成功拯救自己
+        SAVE_HOOK = null;
+        System.gc();
+        //因为finapze方法优先级很低，所以暂停0.5秒以等待它
+        Thread.sleep(500);
+        if (SAVE_HOOK != null) {
+            SAVE_HOOK.isAlive();
+        } else {
+            System.out.println("no, i am dead :(");
+        }
+    }
+}
---
+++ myflx-validation/src/test/java/Lekou.java
@@ -0,0 +1,63 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+/**
+ * 算法语言无关性
+ * 忘掉语言，使用最基础的，没有则自己构建
+ */
+public class Lekou {
+
+
+    /**
+     * 你可以假设给定的 k 总是合理的，且 1 ≤ k ≤ 数组中不相同的元素的个数。
+     * 你的算法的时间复杂度必须优于 O(n log n) , n 是数组的大小。
+     * 题目数据保证答案唯一，换句话说，数组中前 k 个高频元素的集合是唯一的。
+     * 你可以按任意顺序返回答案。
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/top-k-frequent-elements
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     *
+     * hash表计数，顶堆取值/快速排序取值
+     * @param nums nums
+     * @param k    k
+     * @return arr
+     */
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> occurrences = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            occurrences.put(num, occurrences.getOrDefault(num, 0) + 1);
+        }
+
+        // int[] 的第一个元素代表数组的值，第二个元素代表了该值出现的次数
+        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            public int compare(int[] m, int[] n) {
+                return m[1] - n[1];
+            }
+        });
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        int[] ret = new int[k];
+        for (int i = 0; i < k; ++i) {
+            ret[i] = queue.poll()[0];
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = new Lekou().topKFrequent(new int[]{0, 1, 0, 0, 0, 3, 3, 3, 3, 1, 1, 1, 1}, 2);
+        System.out.println(Arrays.toString(ints));
+    }
+}
