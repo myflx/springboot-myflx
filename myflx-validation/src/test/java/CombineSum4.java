d042de6a88b4e96be69bc51e96af349a7d66d031
luoshanglin <ShanglinLuo@sf-express.com>
2020-09-14 18:36:57 +0800

    增加动态回归和记忆搜索

--- myflx-validation/src/test/java/CombineSum4.java
+++ myflx-validation/src/test/java/CombineSum4.java
@@ -1,18 +1,69 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class CombineSum4 {
 
     public static void main(String[] args) {
-        System.out.println(new CombineSum4().combinationSum4(new int[]{1, 2, 3}, 4));
+        System.out.println(new CombineSum4().combinationSum4V2(new int[]{1, 2, 3, 4}, 4));
+    }
+
+
+    /**
+     * 动态规划
+     */
+    public int combinationSum4V3(int[] nums, int target) {
+        if (nums.length == 1) {
+            return target % nums[0] == 0 ? 1 : 0;
+        }
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                    System.out.println(Arrays.toString(dp));
+                }
+            }
+        }
+        return dp[target];
+    }
+
+
+    int[] cache;
+
+    /**
+     *
+     * dp[i] = dp[i-1]
+     * 记忆搜索
+     * [-1, 1, 2, 4, 8]
+     * [-1, 1, 2, 4, 7]
+     */
+    public int combinationSum4V2(int[] nums, int target) {
+        cache = new int[target + 1];
+        Arrays.fill(cache, -1);
+        return memorySearch(nums, target);
+    }
+
+    private int memorySearch(int[] nums, int target) {
+        if (cache[target] != -1) {
+            return cache[target];
+        }
+        if (target == 0) {
+            return 1;
+        }
+        int count = 0;
+        for (int num : nums) {
+            if (num > target) {
+                break;
+            }
+            count += memorySearch(nums, target - num);
+        }
+        cache[target] = count;
+        System.out.println(Arrays.toString(cache));
+        return count;
     }
 
     /**
-     * 2+
+     * 简单递归
      * 给定一个由正整数组成且不存在重复数字的数组，找出和为给定目标正整数的组合的个数。
      *
      * @param nums   [1, 2, 3]
@@ -20,7 +71,7 @@ public class CombineSum4 {
      * @return 所有可能的组合为：
      * (1, 1, 1, 1)
      * (2, 2)
-     *
+     * <p>
      * (1, 1, 2)
      * (1, 2, 1)
      * (1, 3)
@@ -37,43 +88,25 @@ public class CombineSum4 {
         if (len == 0) {
             return 0;
         }
-        // 排序
         Arrays.sort(nums);
-
-        //记录不能整除的余数
-        Map<Integer, Integer> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        // 找出无组合解个数和数据池的公约数
-        for (int num : nums) {
-            if (num > target) {
-                break;
-            }
-            if (target % num == 0) {
-                list.add(num);
-            } else {
-                map.put(num, target / num);
-            }
-        }
-        System.out.println("当前数据池中的目标值的公约数：" + list);
-        System.out.println("当前数据池中的目标值的余数：" + map);
-        AtomicInteger count = new AtomicInteger(list.size());
-
-
-        /*dfs(nums, 0, target, count);*/
-        return count.get();
+        return dfs(nums, 0, target);
     }
 
-    private void dfs(int[] candidates, int i, int target, AtomicInteger count) {
+    private int dfs(int[] candidates, int i, int target) {
         if (i == candidates.length) {
-            return;
+            return 0;
         }
         if (target == 0) {
-            count.incrementAndGet();
-            return;
+            return 1;
         }
-        dfs(candidates, i + 1, target, count);
-        if (target >= candidates[i]) {
-            dfs(candidates, 0, target - candidates[i], count);
+        int count = 0;
+        for (int j = i; j < candidates.length; j++) {
+            if (candidates[j] > target) {
+                break;
+            }
+            count += dfs(candidates, 0, target - candidates[j]);
         }
+
+        return count;
     }
 }
