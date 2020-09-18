b34d760bcb8981c2742954a78062fc84a9e54a1e
luoshanglin <ShanglinLuo@sf-express.com>
2020-09-18 18:34:13 +0800

    reverse

--- myflx-validation/src/test/java/IntegerReverse.java
+++ myflx-validation/src/test/java/IntegerReverse.java
@@ -1,22 +1,43 @@
-import java.util.Objects;
+import java.util.Arrays;
 
 public class IntegerReverse {
 
     public static void main(String[] args) {
-        int reverse = new IntegerReverse().reverse(123);
-        System.out.println(Objects.equals(reverse, 321));
-        StringBuilder str = new StringBuilder("123");
-        System.out.println(str.reverse());
+        System.out.println(Integer.toBinaryString(654321000));
+        System.out.println(Integer.toBinaryString(123456));
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        new IntegerReverse().reverse(2147483647);
     }
 
     public int reverse(int x) {
-        int pre = 1;
-        int max = Integer.MAX_VALUE;
-        if (x<0){
+        byte[] binaryArr = new byte[32];
+        if (x < 0) {
             x = Math.abs(x);
-            pre = -1;
+            binaryArr[0] = 1;
         }
-        StringBuilder str = new StringBuilder(x + "");
-        return 0;
+        StringBuilder str = new StringBuilder(String.valueOf(x));
+        boolean allSame = true;
+        for (int i = 1; i < str.length(); i++) {
+            if (!str.substring(i, i + 1).equals(str.substring(0, 1))) {
+                allSame = false;
+                break;
+            }
+        }
+        if (allSame) {
+            return binaryArr[0] == 1 ? 0 - x : x;
+        }
+        str.reverse();
+        while (str.substring(0, 1).equals("0")) {
+            str.deleteCharAt(0);
+        }
+        if (str.length() == 10 && (int) str.charAt(0) > 2) {
+            return 0;
+        }
+
+        int k = 0;
+
+        //处理翻转
+        return binaryArr[0] == 1 ? 0 - x : x;
     }
 }
