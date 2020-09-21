2ec5ae35875a06fe1a4a03bcc47338b3f9173415
luoshanglin <ShanglinLuo@sf-express.com>
2020-09-21 18:55:16 +0800

    z字转换

--- myflx-validation/src/test/java/StringDealer.java
+++ myflx-validation/src/test/java/StringDealer.java
@@ -1,8 +1,11 @@
 public class StringDealer {
 
     public static void main(String[] args) {
-        System.out.println(new StringDealer().longestCommonSubsequence("pmjghexybyrgzczy"
-                , "hafcdqbgncrcbihkd"));
+        /*System.out.println(new StringDealer().longestCommonSubsequence("pmjghexybyrgzczy"
+                , "hafcdqbgncrcbihkd"));*/
+        /*System.out.println(new StringDealer().convert("LEE", 3).equals("LEE"));
+        System.out.println(new StringDealer().convert("LEETCODEISHIRING", 3).equals("LCIRETOESIIGEDHN"));*/
+        System.out.println(new StringDealer().convert("LEETCODEISHIRING", 4).equals("LDREOEIIECIHNTSG"));
     }
 
     public int longestCommonSubsequence(String text1, String text2) {
@@ -15,8 +18,8 @@ public class StringDealer {
             for (int i2 = 1; i2 < j + 1; i2++) {
                 if (text1.charAt(i1 - 1) == text2.charAt(i2 - 1)) {
                     dpTables[i1][i2] = dpTables[i1 - 1][i2 - 1] + 1;
-                }else {
-                    dpTables[i1][i2] = Math.max(dpTables[i1][i2-1],dpTables[i1-1][i2]);
+                } else {
+                    dpTables[i1][i2] = Math.max(dpTables[i1][i2 - 1], dpTables[i1 - 1][i2]);
                 }
             }
         }
@@ -35,5 +38,26 @@ public class StringDealer {
         } else {
             return Math.max(dp(text1, text2, i - 1, j), dp(text1, text2, i, j - 1));
         }
+    }
+
+    /**
+     * z子形转换
+     */
+    public String convert(String s, int numRows) {
+        if (s.length() <= numRows) {
+            return s;
+        }
+        int maxLen = 2 * (numRows - 1);
+        StringBuilder rev = new StringBuilder();
+        for (int i = 0; i < numRows; i++) {
+            int index = i;
+            int j = maxLen - i;
+            int currentLen = j - i == 0 ? maxLen : j - i;
+            while (index < s.length()) {
+                rev.append(s.charAt(index));
+                index += currentLen;
+            }
+        }
+        return rev.toString();
     }
 }
