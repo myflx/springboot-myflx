8fb35a27f66d8209bbddecf64f8bbffaba6a2ef9
luoshanglin <ShanglinLuo@sf-express.com>
2020-09-21 13:00:54 +0800

    整数翻转处理

--- myflx-validation/src/test/java/IntegerReverse.java
+++ myflx-validation/src/test/java/IntegerReverse.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.List;
 
 public class IntegerReverse {
 
@@ -6,31 +7,58 @@ public class IntegerReverse {
         System.out.println(Integer.toBinaryString(654321000));
         System.out.println(Integer.toBinaryString(123456));
         System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
-        new IntegerReverse().reverse(2147483647);
+        System.out.println(Integer.MIN_VALUE);//2147483647
+        System.out.println(new IntegerReverse().reverse(-2147447412));
     }
 
+    private static final int UP = Integer.MAX_VALUE / 10;
+    private static final int DOWN = Integer.MIN_VALUE / 10;
+
     public int reverse(int x) {
-        byte[] binaryArr = new byte[32];
-        if (x < 0) {
-            x = Math.abs(x);
-            binaryArr[0] = 1;
+        int rev = 0;
+        while (x / 10 != 0) {
+            rev = rev * 10 + x % 10;
+            x /= 10;
         }
-        StringBuilder str = new StringBuilder(String.valueOf(x));
+        if (rev > UP || rev < DOWN) {
+            return 0;
+        }
+        return rev * 10 + x % 10;
+    }
+
+    private static final List<Integer> list = Arrays.asList(2, 1, 4, 7, 4, 8, 3, 6, 4, 7);
+
+
+    public int reverse2(int x) {
+        boolean hasPre = false;
+        final StringBuilder str = new StringBuilder(String.valueOf(x));
+        if (str.charAt(0) == '-') {
+            str.deleteCharAt(0);
+            hasPre = true;
+        }
         str.reverse();
         if (str.toString().equals(String.valueOf(x))) {
-            return binaryArr[0] == 1 ? -x : x;
+            return hasPre ? -x : x;
         }
         while (str.substring(0, 1).equals("0")) {
             str.deleteCharAt(0);
         }
-        if (str.length() == 10 && (int) str.charAt(0) > 2) {
-            return 0;
+        if (str.length() == 10) {
+            int i = 0;
+            while (i < str.length()) {
+                if (i == 10 && hasPre && Integer.valueOf(str.substring(i, i + 1)) > list.get(i) + 1) {
+                    return 0;
+                }
+                if (Integer.valueOf(str.substring(i, i + 1)) > list.get(i)) {
+                    return 0;
+                } else if (Integer.valueOf(str.substring(i, i + 1)) < list.get(i)) {
+                    break;
+                }
+                i++;
+            }
         }
-
-        int k = 0;
-
-        //处理翻转
-        return binaryArr[0] == 1 ? -x : x;
+        //处理符号
+        x = Integer.parseInt(str.toString());
+        return hasPre ? -x : x;
     }
 }
