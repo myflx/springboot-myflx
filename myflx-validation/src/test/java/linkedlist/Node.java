5ed6b4e027efb59d4a63fbf7886019dc3e083a3e
luoshanglin <ShanglinLuo@sf-express.com>
2020-11-12 13:38:17 +0800

    深度复制

---
+++ myflx-validation/src/test/java/linkedlist/Node.java
@@ -0,0 +1,25 @@
+package linkedlist;
+
+public class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+
+    public Node(int val, Node next) {
+        this.val = val;
+        this.next = next;
+    }
+
+    public Node(int val, Node next, Node random) {
+        this.val = val;
+        this.next = next;
+        this.random = random;
+    }
+}
+
