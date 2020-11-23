0d18a55da0046a600578d7d8680c1063af339de3
luoshanglin <ShanglinLuo@sf-express.com>
2020-11-23 18:40:22 +0800

    转换为罗马数字（优先使用数组结构）

--- myflx-validation/src/test/java/number/Solution.java
+++ myflx-validation/src/test/java/number/Solution.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
-        System.out.println(Integer.toBinaryString(1));
+        /*System.out.println(Integer.toBinaryString(1));
         System.out.println(Integer.toBinaryString(3));
         System.out.println(Integer.toBinaryString(7));
         System.out.println(Integer.toBinaryString(15));
@@ -20,7 +20,30 @@ public class Solution {
         System.out.println(Integer.toBinaryString(15 & 100));
         System.out.println(Integer.toBinaryString(31 & 100));
         System.out.println(Integer.toBinaryString(63 & 100));
-        System.out.println(Integer.toBinaryString(127 & 100));
+        System.out.println(Integer.toBinaryString(127 & 100));*/
+
+        System.out.println(new Solution().intToRoman(1994));
+    }
+
+
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    /**
+     * https://leetcode-cn.com/problems/integer-to-roman/
+     */
+    public String intToRoman(int num) {
+        StringBuilder ret = new StringBuilder();
+        int current = num;
+        int boundIndex = 0;
+        while (current > 0) {
+            while (current < values[boundIndex]) {
+                boundIndex++;
+            }
+            ret.append(symbols[boundIndex]);
+            current -= values[boundIndex];
+        }
+        return ret.toString();
     }
 
     public List<List<Integer>> threeSum(int[] nums) {
